55ad480471d427fc58db16b1790a1ce0cd07e13f
GrandToast <ondravo@gmail.com>
2013-08-27 22:05:13 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy
+
 	validates :name, presence: true,
 					 length:   { maximum: 50 }
 
@@ -19,6 +21,11 @@ class User < ActiveRecord::Base
 
 	  def User.encrypt(token)
 	    Digest::SHA1.hexdigest(token.to_s)
+	  end
+
+	  def feed
+	    # This is preliminary. See "Following users" for the full implementation.
+	    Micropost.where("user_id = ?", id)
 	  end
 
 	  private
